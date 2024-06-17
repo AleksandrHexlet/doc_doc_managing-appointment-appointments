dcfd36dcb6a21941dcd4c764c8c4000007df3798
Alexandr <AlexandrDHL@yandex.ru>
2024-06-17 19:51:10 +0300

    init commit

---
+++ src/main/java/com/docdoc/doc_doc_managingappointmentappointments/DocDocManagingAppointmentAppointmentsApplication.java
@@ -0,0 +1,13 @@
+package com.docdoc.doc_doc_managingappointmentappointments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocDocManagingAppointmentAppointmentsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocDocManagingAppointmentAppointmentsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/docdoc/doc_doc_managingappointmentappointments/DocDocManagingAppointmentAppointmentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docdoc.doc_doc_managingappointmentappointments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocDocManagingAppointmentAppointmentsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
